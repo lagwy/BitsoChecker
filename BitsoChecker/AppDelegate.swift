8ff9b09c43387dc46a94ac5101a5148d03f8685a
Luis Ángel Martínez <luis.0795@hotmail.com>
2017-12-18 12:49:33 -0600

    window hide on launch

--- BitsoChecker/AppDelegate.swift
+++ BitsoChecker/AppDelegate.swift
@@ -10,18 +10,39 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     @IBOutlet weak var window: NSWindow!
-
-
+    
+    var item : NSStatusItem? = nil
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        item = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+        
+        //Displays text on menu bar
+        item?.title = "TestMe"
+        
+        // Defines function to run when the user clicks on the text on menu bar
+        //item?.action = #selector(AppDelegate.testMe)
+        
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Option 1", action: #selector(AppDelegate.testMe), keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.quitMe), keyEquivalent: ""))
+        item?.menu = menu
     }
-
+    
+    func testMe() {
+        print ("It works!")
+    }
+    
+    func quitMe() {
+        NSApplication.shared().terminate(self)
+    }
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    
 }
-
